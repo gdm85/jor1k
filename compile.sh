8d7d1959a63782f0ca581a57c00799bbc20a05bb
gdm85 <gdm85@users.noreply.github.com>
2016-03-29 23:24:10 +0200

    Proper shell script semantics

--- compile.sh
+++ compile.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
+
+set -e
+
 browserify -r ./js/plugins/terminal-linux.js:LinuxTerm -r ./js/master/master.js:Jor1k -o demos/jor1k-master-min.js
 browserify js/worker/worker.js -o demos/jor1k-worker-min.js
 
